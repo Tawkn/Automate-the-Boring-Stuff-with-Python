b8e0ebe2bd4627e414c21b2712ad2e383f890698
Tawkn <50852825+Tawkn@users.noreply.github.com>
2019-05-29 15:11:58 -0400

    Update Collatz Sequence.py

--- Chapter 3/Collatz Sequence.py
+++ Chapter 3/Collatz Sequence.py
@@ -1,21 +1,26 @@
-print('Welcome to the Collatz Sequence!')
-print('If I were a betting a man, I would bet that starting with integer > 1 you choose, the last return value will be 1!')
-print('..Eventually :)')
-print('Enter your number!')
+# This is a Collatz Sequence
+# if a Negative Even Number is entered, it will test the IF function once but then output the ELIF function
+
+print('Enter an integer!!')
 
 number = int(input())
 
-while number != 1:
-    if number % 2 == 0:
-        number = number // 2
-        print(number)
-    elif number <= 0:
-        print("Nope. I've had enough. Crashing right meow. ZzzZzzZzzz")
-        break
-    else:
-        number = 3 * number + 1
-        print(number)
+def collatz(number):
+    while number != 1:
+        if number % 2 == 0:
+            number = number // 2
+            print(number)
+        elif number < 0:
+            print("Nope. I've had enough. Crashing right meow. ZzzZzzZzzz")
+            break
+        else:
+            number = 3 * number + 1
+            print(number)
 
+try:
+    collatz(number)
+except ValueError:
+    print('You suck.')
 
 
 
